6f6b9cf8df3ac5bcc1b4e19aac6fae4380893a14
harpekar <karenl.harper88@gmail.com>
2018-05-13 17:06:33 -0700

    Added test.py

---
+++ test.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" Author: Ben Lester 
+    Electrical and Computer Engineering Senior Capstone
+    Oregon State University
+"""
+
+from dronekit import connect, VehicleMode, LocationGlobal, LocationGlobalRelative
+from pymavlink import mavutil
+import time
+import math
+#import picamera
+
+# Function to arm and takeoff copter
+# Monitors alititude and confirms alitude before falling out of function
+
+def arm_and_takeoff(vehicle, aTargetAltitude):
+    """
+    Arms vehicle and fly to aTargetAltitude.
+    """
+
+    print "Basic pre-arm checks"
+    # Don't try to arm until autopilot is ready
+    while not vehicle.is_armable:
+        print " Waiting for vehicle to initialise..."
+        time.sleep(1)
+
+        
+    print "Arming motors"
+    # Copter should arm in GUIDED mode
+    vehicle.mode = VehicleMode("GUIDED")
+    vehicle.armed = True    
+
+    # Confirm vehicle armed before attempting to take off
+    while not vehicle.armed:      
+        print " Waiting for arming..."
+        time.sleep(1)
+
+    print "Taking off!"
+    vehicle.simple_takeoff(aTargetAltitude) # Take off to target altitude
+
+    # Wait until the vehicle reaches a safe height before processing the goto (otherwise the command 
+    #  after Vehicle.simple_takeoff will execute immediately).
+    while True:
+        print " Altitude: ", vehicle.location.global_relative_frame.alt 
+        #Break and return from function just below target altitude.        
+        if vehicle.location.global_relative_frame.alt>=aTargetAltitude*0.95: 
+            print "Reached target altitude"
+            break
+        time.sleep(1)
+
+# Function to yaw the copter
+def condition_yaw(heading, vehicle, relative=True):
+    if relative:
+        is_relative=1 #yaw relative to direction of travel
+    else:
+        is_relative=0 #yaw is an absolute angle
+    # create the CONDITION_YAW command using command_long_encode()
+    msg = vehicle.message_factory.command_long_encode(
+        0, 0,    # target system, target component
+        mavutil.mavlink.MAV_CMD_CONDITION_YAW, #command
+        0, #confirmation
+        heading,    # param 1, yaw in degrees
+        0,          # param 2, yaw speed deg/s
+        1,          # param 3, direction -1 ccw, 1 cw
+        is_relative, # param 4, relative offset 1, absolute angle 0
+        0, 0, 0)    # param 5 ~ 7 not used
+    # send command to vehicle
+    vehicle.send_mavlink(msg)
+
+# Function to adjust yaw and trigger camera for full 360 degrees
+def collect_images(num_images, vehicle):
+   
+    image_number = 10 		# Number of images to be collected
+    degrees = 360/image_number	# Heading offset in degrees per image
+    heading = 0			# Innitial heading in degrees
+
+    for i in range (1,image_number):
+        condition_yaw(heading, vehicle)
+        #camera.capture('img%s.jpg' % i)
+        print 'Image %s saved' % i
+        time.sleep(3) 
+        heading = heading + degrees
+	
+# Function to set velocity to zero - required before the yaw command will work
+def send_global_velocity(velocity_x, velocity_y, velocity_z, vehicle):
+    """
+    Move vehicle in direction based on specified velocity vectors.
+    """
+    msg = vehicle.message_factory.set_position_target_global_int_encode(
+        0,       # time_boot_ms (not used)
+        0, 0,    # target system, target component
+        mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT_INT, # frame
+        0b0000111111000111, # type_mask (only speeds enabled)
+        0, # lat_int - X Position in WGS84 frame in 1e7 * meters
+        0, # lon_int - Y Position in WGS84 frame in 1e7 * meters
+        0, # alt - Altitude in meters in AMSL altitude(not WGS84 if absolute or relative)
+        # altitude above terrain if GLOBAL_TERRAIN_ALT_INT
+        velocity_x, # X velocity in NED frame in m/s
+        velocity_y, # Y velocity in NED frame in m/s
+        velocity_z, # Z velocity in NED frame in m/s
+        0, 0, 0, # afx, afy, afz acceleration (not supported yet, ignored in GCS_Mavlink)
+        0, 0)    # yaw, yaw_rate (not supported yet, ignored in GCS_Mavlink)
+   
+    vehicle.send_mavlink(msg)
+    time.sleep(1)
+
+#Set connection string  
+connection_string = '127.0.0.1:14550'
+
+# Connect to the Vehicle
+print 'Connecting to vehicle on: %s' % connection_string
+vehicle = connect(connection_string, wait_ready=True)
+	
+print "Place aircraft in launch location"
+time.sleep(5)
+print "Taking off in: "
+for i in range (0,10):
+	print 10-i
+	time.sleep(1)
+
+arm_and_takeoff(vehicle, 10)
+
+send_global_velocity(0,0,0,vehicle)
+
+collect_images(6, vehicle)
+print "Returning to Launch"
+#vehicle.parameters['RTL_ALT'] = 0;
+vehicle.mode = VehicleMode("RTL")
+
+#Close vehicle object before exiting script
+print "Close vehicle object"
+vehicle.close()
+          
