d55d9c6f80bc8ad4ba8e1bf15388ad7aad2e104f
Karen <karenl.harper88@gmail.com>
2018-07-05 15:32:54 -0700

    Fixed panorama_convert.sh

--- panorama_convert.sh
+++ panorama_convert.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
+
 python usercode.py
 
-convert img*.jpg +append panorama.bmp
+convert img*.jpg +append -resize 'x320' panorama.bmp
 mogrify -flip panorama.bmp
 
 ../BMPtoFramebuffer/bmpread panorama.bmp /dev/fb1
